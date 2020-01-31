659f29ce581451bf62ac05bd126f7428ffd515d9
David <dcornish84@gmail.com>
2020-01-31 15:22:10 -0600

    seeding restaurants

--- FriendlyFood/FriendlyFood/Data/ApplicationDbContext.cs
+++ FriendlyFood/FriendlyFood/Data/ApplicationDbContext.cs
@@ -60,7 +60,95 @@ namespace FriendlyFood.Data
         user.PasswordHash = passwordHash.HashPassword(user, "Admin8*");
         modelBuilder.Entity<ApplicationUser>().HasData(user);
 
-    }
+        //create some restaurants
+            Restaurant restaurant1 = new Restaurant
+            {
+                Id = 1,
+                RestaurantName = "Wild Cow",
+                Address = "1100 Fatherland St SUITE 104",
+                ZipCode = 37206,
+                CuisineId = 1,
+                ApplicationUserId = user.Id
+            };
+            modelBuilder.Entity<Restaurant>().HasData(restaurant1);
+
+            Restaurant restaurant2 = new Restaurant
+            {
+                Id = 2,
+                RestaurantName = "Graze",
+                Address = "1888 Eastland Ave",
+                ZipCode = 37206,
+                CuisineId = 2,
+                ApplicationUserId = user.Id
+            };
+            modelBuilder.Entity<Restaurant>().HasData(restaurant2);
+
+            Restaurant restaurant3 = new Restaurant
+            {
+                Id = 3,
+                RestaurantName = "Woodlands",
+                Address = "3415 West End Ave",
+                ZipCode = 37203,
+                CuisineId = 3,
+                ApplicationUserId = user.Id
+            };
+            modelBuilder.Entity<Restaurant>().HasData(restaurant3);
+
+            Restaurant restaurant4 = new Restaurant
+            {
+                Id = 4,
+                RestaurantName = "City House",
+                Address = "1222 4th Ave N",
+                ZipCode = 37208,
+                CuisineId = 4,
+                ApplicationUserId = user.Id
+            };
+            modelBuilder.Entity<Restaurant>().HasData(restaurant4);
+
+            Restaurant restaurant5 = new Restaurant
+            {
+                Id = 5,
+                RestaurantName = "Farm Burger",
+                Address = "4013 Charlotte Ave",
+                ZipCode = 37209,
+                CuisineId = 5,
+                ApplicationUserId = user.Id
+            };
+            modelBuilder.Entity<Restaurant>().HasData(restaurant5);
+
+            Restaurant restaurant6 = new Restaurant
+            {
+                Id = 6,
+                RestaurantName = "Love Peace & Pho",
+                Address = "2112 8th Ave S",
+                ZipCode = 37204,
+                CuisineId = 6,
+                ApplicationUserId = user.Id
+            };
+            modelBuilder.Entity<Restaurant>().HasData(restaurant6);
+
+            Restaurant restaurant7 = new Restaurant
+            {
+                Id = 7,
+                RestaurantName = "San Antonio Taco Co",
+                Address = "416 21st Ave S",
+                ZipCode = 37203,
+                CuisineId = 7,
+                ApplicationUserId = user.Id
+            };
+            modelBuilder.Entity<Restaurant>().HasData(restaurant7);
+
+            Restaurant restaurant8 = new Restaurant
+            {
+                Id = 8,
+                RestaurantName = "Two Boots Nashville",
+                Address = "1925 Broadway",
+                ZipCode = 37203,
+                CuisineId = 8,
+                ApplicationUserId = user.Id
+            };
+            modelBuilder.Entity<Restaurant>().HasData(restaurant8);
+        }
 
     }
 }
